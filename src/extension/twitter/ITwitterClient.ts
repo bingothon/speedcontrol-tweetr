717e24ffa3200f8ce80cc4425e22b4a48858e408
duncte123 <contact@duncte123.me>
2023-07-14 09:09:47 +0200

    Have the linter complain less

--- src/extension/twitter/ITwitterClient.ts
+++ src/extension/twitter/ITwitterClient.ts
@@ -1,6 +1,8 @@
 import { SendTweetV2Params } from 'twitter-api-v2/dist/types';
 
-export default interface ITwitterClient {
+interface ITwitterClient {
   uploadMedia(file: string): Promise<string>;
   tweet(text: string, params: Partial<SendTweetV2Params> | undefined): Promise<void>;
 }
+
+export default ITwitterClient;
